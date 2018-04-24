b5e598e8b1c58ede08657e7c70cf9e30a68d5ea8
VBurt <ChargerIIC@yahoo.com>
2018-04-24 13:46:04 -0700

    Intial Version of About Page
    Will need to be refactored into two seperate components and dressed up

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'page-about',
@@ -7,8 +8,29 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+public userData : string;
+public repos: string;
+public avatarUri: string;
+public profileUri: string;
+public followers: string;
+public following: string;
+public memberSince: string;
 
+
+  constructor(public navCtrl: NavController, public http: Http) {
+    //TODO: Break into Component
+    http.get('https://api.github.com/users/ChargerIIC')
+    .map(r => {
+      var data = r.json();
+      this.userData = data;
+      this.repos = data.public_repos;
+      this.avatarUri = data.avatar_url;
+      this.profileUri = data.url;
+      this.followers = data.followers;
+      this.following = data.following;
+      this.memberSince = data.created_at;
+      return this.userData;
+  }).subscribe(console.log);
   }
 
 }
